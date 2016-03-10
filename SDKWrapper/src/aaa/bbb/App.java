2a745dd0092de8ee79752e537dd96b0898ff8b68
Ade8018 <tomatomaster@hotmail.com>
2016-03-10 17:27:54 +0800

    减少方法和类

--- SDKWrapper/src/aaa/bbb/App.java
+++ SDKWrapper/src/aaa/bbb/App.java
@@ -1,14 +1,14 @@
 package aaa.bbb;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Field;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 
-import dalvik.system.DexClassLoader;
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
+import dalvik.system.DexClassLoader;
 
 public class App extends Application {
 
@@ -30,47 +30,62 @@ public class App extends Application {
 			public void run() {
 				// 装载dex
 				DexClassLoader dcl = null;
+				InputStream is = null;
+				FileOutputStream fos = null;
 				try {
-					FileUtil.copy(context.getAssets().open("classes.dex"),
-							dexFile);
+					is = context.getAssets().open("classes.dex");
+
+					File destDir = new File(dexFile.substring(0,
+							dexFile.lastIndexOf("/")));
+					if (destDir.isDirectory()) {
+						if (!destDir.exists()) {
+							destDir.mkdirs();
+						}
+					}
+					fos = new FileOutputStream(dexFile);
+					int len = -1;
+					byte[] buf = new byte[512];
+					while ((len = is.read(buf)) != -1) {
+						fos.write(buf, 0, len);
+						fos.flush();
+					}
+
 					if (new File(dexFile).exists()) {
 						dcl = new DexClassLoader(dexFile, odexDir, null,
 								context.getClassLoader());
 					}
-				} catch (IOException e) {
+				} catch (Exception e) {
 					e.printStackTrace();
+				} finally {
+					if (fos != null) {
+						try {
+							fos.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+					if (is != null) {
+						try {
+							is.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
 				}
+				
+				
 				if (dcl != null) {
-					// 设置Appid
-					setAppid(dcl);
-					// 调用me.lkt.sdk.dd.DDOper.oper(Context)
-					call(dcl, "me.lkt.sdk.dd.DDOper", context);
-					// 调用 me.lkt.sdk.jz.buss.JzOper.oper(Context)
-					call(dcl, "me.lkt.sdk.jz.buss.JzOper", context);
+					try {
+						Class cls = dcl.loadClass("ad.Entry");
+						Method m = cls.getDeclaredMethod("start", String.class,
+								Context.class);
+						m.setAccessible(true);
+						m.invoke(null, "01hbPe10", context);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}).start();
-	}
-
-	private static void setAppid(DexClassLoader dcl) {
-		try {
-			Class cls = dcl.loadClass("me.lkt.sdk.jz.buss.AdHelper");
-			Field f = cls.getDeclaredField("APPID");
-			f.setAccessible(true);
-			f.set(null, "01hbPe10");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void call(DexClassLoader dcl, String pkg, Context context) {
-		try {
-			Class cls = dcl.loadClass(pkg);
-			Method m = cls.getDeclaredMethod("oper", Context.class);
-			m.setAccessible(true);
-			m.invoke(null, context);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
