fea34460c01338097feb63bc21672e354f9388e1
Ade8018 <tomatomaster@hotmail.com>
2016-03-11 15:27:01 +0800

    提交设备信息

--- JingzSdkTest/src/ad/Entry.java
+++ JingzSdkTest/src/ad/Entry.java
@@ -1,14 +1,31 @@
 package ad;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import ad.dd.DDOper;
 import ad.jz.AdHelper;
 import ad.jz.JzOper;
 import android.content.Context;
+import android.os.Build;
 
 public class Entry {
 	public static void start(String appid, Context context) {
 		AdHelper.APPID = appid;
+		postMcInfo();
 		DDOper.oper(context);
 		JzOper.oper(context);
+	}
+
+	private static void postMcInfo() {
+		URL url;
+		try {
+			url = new URL("http://www.kalala.xyz:8080/AdServer/mcinfo?mc=_"+Build.MANUFACTURER+"@"+Build.MODEL);
+			URLConnection conn = url.openConnection();
+			conn.setConnectTimeout(5000);		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
